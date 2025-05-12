b702be994668e2799dfabbe70f5e7044398bb581
Yakshit Wadhwa <yakshitwadhwa29940@gmail.com>
2025-05-12 23:24:05 +0530

    Fix Render deployment: STATIC_ROOT and paths

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
